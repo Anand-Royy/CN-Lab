cbc51a8bcac837358e60391d4267965fcd73fc8f
Anand Roy <73389636+Anand-Royy@users.noreply.github.com>
2023-06-25 23:49:54 +0530

    Update selective.cpp

--- selective.cpp
+++ selective.cpp
@@ -1,49 +1,43 @@
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-  srand(time(NULL));
-
-  int totalFrames;
-  cout << "Enter the total number of frames: ";
-  cin >> totalFrames;
-
-  int windowSize;
-  cout << "Enter the window size: ";
-  cin >> windowSize;
-
-  int base = 1;
-  int nextSeqNum = 1;
-  int transmittedFrames = 0;
-
-  while (transmittedFrames < totalFrames) {
-    for (int i = base; i < base + windowSize && i <= totalFrames; i++) {
-      cout << "Sending Frame " << i << "..." << endl;
-    }
-
-    int acknowledgement = rand() % 2; // Simulating random acknowledgement (0:
-                                      // Acknowledged, 1: Not Acknowledged)
-
-    if (acknowledgement == 0) {
-      int numAckedFrames = rand() % windowSize +
-                           1; // Simulating random number of frames acknowledged
-      for (int k = base; k < numAckedFrames + base; k++)
-        cout << "Frame " << k << " Acknowledged." << endl;
-
-      transmittedFrames += numAckedFrames;
-      base += numAckedFrames;
-      nextSeqNum += numAckedFrames;
-    } else {
-      cout << "Frames Not Acknowledged. Retransmitting Window..." << endl;
-    }
-
-    cout << endl;
-  }
-
-  cout << "All frames have been transmitted and acknowledged." << endl;
-
-  return 0;
+bool ack() {
+  // acknowledged or not
+  return rand() % 2;
 }
+
+void solve() {
+  int packets;
+  cin >> packets;
+  int window_size;
+  cin >> window_size;
+  int window_start = 1;
+  set<int> packets_to_be_transmitted;
+
+  while (window_start <= packets) {
+    for (int j = window_start; j < window_start + window_size && j <= packets;
+         ++j) {
+      cout << "Sending packet No : " << j << "\n";
+    }
+    for (int j = window_start; j < window_start + window_size && j <= packets;
+         ++j) {
+      if (ack()) {
+        cout << "Packet No : " << j << " acknowledged \n";
+      } else {
+        cout << "Failed to acknowledge packet : " << j << "\n";
+        packets_to_be_transmitted.insert(j);
+      }
+    }
+    while (!packets_to_be_transmitted.empty()) {
+      auto f = packets_to_be_transmitted.begin();
+      if (ack()) {
+        cout << "Resending Packet No : " << *f << " acknowledged \n";
+        packets_to_be_transmitted.erase(f);
+      } else {
+        cout << "Failed to acknowledge packet : " << *f << "\n";
+      }
+    }
+    window_start += window_size;
+  }
+}
+
+int main() { solve(); }
