207311131ece548031b836bd6c1c9ec0da825302
Anand Roy <73389636+Anand-Royy@users.noreply.github.com>
2023-06-25 16:33:46 +0530

    Update selective.cpp

--- selective.cpp
+++ selective.cpp
@@ -1,46 +1,49 @@
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    srand(time(NULL));
-    
-    int totalFrames;
-    cout << "Enter the total number of frames: ";
-    cin >> totalFrames;
-    
-    int windowSize;
-    cout << "Enter the window size: ";
-    cin >> windowSize;
-    
-    int base = 1;
-    int nextSeqNum = 1;
-    int transmittedFrames = 0;
-    
-    while (transmittedFrames < totalFrames) {
-        for (int i = base; i < base + windowSize && i <= totalFrames; i++) {
-            cout << "Sending Frame " << i << "..." << endl;
-        }
-        
-        int acknowledgement = rand() % 2; // Simulating random acknowledgement (0: Acknowledged, 1: Not Acknowledged)
-        
-        if (acknowledgement == 0) {
-            int numAckedFrames = rand() % windowSize + 1; // Simulating random number of frames acknowledged
-            cout << numAckedFrames << " frame(s) Acknowledged." << endl;
-            
-            transmittedFrames += numAckedFrames;
-            base += numAckedFrames;
-            nextSeqNum += numAckedFrames;
-        } else {
-            cout << "Frames Not Acknowledged. Retransmitting Window..." << endl;
-        }
-        
-        cout << endl;
+  srand(time(NULL));
+
+  int totalFrames;
+  cout << "Enter the total number of frames: ";
+  cin >> totalFrames;
+
+  int windowSize;
+  cout << "Enter the window size: ";
+  cin >> windowSize;
+
+  int base = 1;
+  int nextSeqNum = 1;
+  int transmittedFrames = 0;
+
+  while (transmittedFrames < totalFrames) {
+    for (int i = base; i < base + windowSize && i <= totalFrames; i++) {
+      cout << "Sending Frame " << i << "..." << endl;
     }
-    
-    cout << "All frames have been transmitted and acknowledged." << endl;
-    
-    return 0;
+
+    int acknowledgement = rand() % 2; // Simulating random acknowledgement (0:
+                                      // Acknowledged, 1: Not Acknowledged)
+
+    if (acknowledgement == 0) {
+      int numAckedFrames = rand() % windowSize +
+                           1; // Simulating random number of frames acknowledged
+      for (int k = base; k < numAckedFrames + base; k++)
+        cout << "Frame " << k << " Acknowledged." << endl;
+
+      transmittedFrames += numAckedFrames;
+      base += numAckedFrames;
+      nextSeqNum += numAckedFrames;
+    } else {
+      cout << "Frames Not Acknowledged. Retransmitting Window..." << endl;
+    }
+
+    cout << endl;
+  }
+
+  cout << "All frames have been transmitted and acknowledged." << endl;
+
+  return 0;
 }
